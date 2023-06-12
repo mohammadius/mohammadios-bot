c23af020c1e98672463a6cd3f48b0852553a199e
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2023-06-12 16:59:33 +0000

    implemented has(key) function

--- redisAdapter.ts
+++ redisAdapter.ts
@@ -1,18 +1,11 @@
 import type { StorageAdapter } from "https://deno.land/x/grammy@v1.16.1/mod.ts";
-import type { Redis } from "https://deno.land/x/redis@v0.30.0/mod.ts";
 import redis from "./redis.ts";
 import { type SessionData, sessionDataZod } from "./types/bot.ts";
 import { initialSession } from "./botHelpers.ts";
 
 export class RedisAdapter implements StorageAdapter<SessionData> {
-	private redis: Redis;
-
-	constructor() {
-		this.redis = redis;
-	}
-
 	async read(key: string) {
-		const session = await this.redis.get(key);
+		const session = await redis.get(key);
 		const result = sessionDataZod.safeParse(JSON.parse(session ?? "0"));
 		if (result.success) {
 			return result.data;
@@ -22,10 +15,15 @@ export class RedisAdapter implements StorageAdapter<SessionData> {
 	async write(key: string, value: SessionData) {
 		const result = sessionDataZod.safeParse(value);
 		JSON.stringify(result.success ? result.data : initialSession());
-		await this.redis.set(key, JSON.stringify(result.success ? result.data : initialSession()));
+		await redis.set(key, JSON.stringify(result.success ? result.data : initialSession()));
 	}
 
 	async delete(key: string) {
-		await this.redis.del(key);
+		await redis.del(key);
+	}
+
+	async has(key: string) {
+		const result = await redis.keys(key);
+		return result.length === 1 && result[0] === key;
 	}
 }
