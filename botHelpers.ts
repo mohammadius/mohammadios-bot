393ea53072e97bb3dbf7860ebb3b1abb1911e5dd
Mohammad Hashemi <mohammad.hashemi@outlook.com>
2023-06-13 19:00:25 +0000

    resized keyboards. they're not huge anymore

--- botHelpers.ts
+++ botHelpers.ts
@@ -8,6 +8,6 @@ export const initialSession: () => SessionData = () => ({
 	youtubeVideos: []
 });
 
-export const songSearchKeyboard = new Keyboard().placeholder("song name and artist").text("Cancel");
+export const songSearchKeyboard = new Keyboard().placeholder("song name and artist").text("Cancel").resized();
 
-export const selectionKeyboard = new Keyboard().text("1").text("2").text("3").text("4").text("5").row().text("Cancel");
+export const selectionKeyboard = new Keyboard().text("1").text("2").text("3").text("4").text("5").row().text("Cancel").resized();
